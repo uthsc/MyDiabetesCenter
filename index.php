f584652424032bdb4cdafcb518d90c8a2d835dbf
nicwinn <nic.liamg@gmail.com>
2015-02-05 08:30:34 -0600

    Replaced the text for the home link with the home icon

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <nav class="top-bar" data-topbar="data-topbar" role="navigation">
         <ul class="title-area">
             <li class="name">
-                <h1><a href="#">Home</a></h1>
+                <h1><a href="#"><i class="fa fa-home fa-2x home-icon"></i></a></h1>
             </li>
             <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
             <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>

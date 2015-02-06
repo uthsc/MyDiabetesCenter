9c759601496d2b740a8ab14d0b40b4f40ffab39d
nicwinn <nic.liamg@gmail.com>
2015-02-06 15:50:28 -0600

    Added a ribbon to bottom of content.

---
+++ ribbon.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html class="no-js" lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>My Diabetes Center</title>
+    <link rel="stylesheet" href="css/mdc.css"/>
+    <link rel="stylesheet" href="bower_components/fontawesome/css/font-awesome.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Short+Stack' rel='stylesheet' type='text/css'>
+    <script src="bower_components/modernizr/modernizr.js"></script>
+</head>
+<body>
+<!-- Header --><!--Hero Image-->
+<header class="hero large-text-left small-text-center">
+    <div class="logo-background">
+        <h1><strong>Diabetes Prevention &amp;&nbsp;Wellness Center</strong></h1>
+
+        <h2 class="hidden-for-small">Overcoming&nbsp;Diabetes One&nbsp;Step&nbsp;at&nbsp;a&nbsp;Time</h2>
+    </div>
+</header>
+<div class="contain-to-grid sticky">
+    <nav class="top-bar" data-topbar="data-topbar" role="navigation">
+        <ul class="title-area">
+            <li class="name">
+                <h1><a href="#"><i class="fa fa-home fa-2x home-icon"></i></a></h1>
+            </li>
+            <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+            <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+        </ul>
+        <section class="top-bar-section">
+            <!-- Right Nav Section -->
+            <ul class="right">
+                <li class="has-dropdown">
+                    <a href="#">Resources</a>
+                    <ul class="dropdown">
+                        <li><a href="#">Patientslikeme</a></li>
+                        <li><a href="#"> My Diabetes Apps</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+            <!-- Left Nav Section -->
+            <ul class="left">
+                <li class="has-dropdown">
+                    <a href="#">About</a>
+                    <ul class="dropdown">
+                        <li><a href="#">Misson, Vision, &amp; Goals</a></li>
+                    </ul>
+                </li>
+
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Get Started</a></li>
+                <li><a href="#">Stories</a></li>
+                <li><a href="#">Ambassadors</a></li>
+            </ul>
+        </section>
+    </nav>
+</div>
+<!-- End Header -->
+<div class="row">
+    <div class="columns panel with-ribbon">
+        <figure class="right image-40">
+            <img src="images/methodist-building.jpg"
+                 alt="Photo of the Methodist South Hospital building"
+                />
+            <figcaption>Methodist South Hospital building. A 156-bed community hospital located in Whitehaven
+                <address>1300 Wesley Dr. Memphis,TN 38116.</address>
+            </figcaption>
+        </figure>
+        <p>This homepage demo was set up using the content from the previous demo sent to me. I moved the "Contact Info" into the template footer so it could appear on all pages. Now this page is basically left with just a photo of the building. If every thing else looks good I can create the other pages based on this and look for new content for this page later. <a href="images/demo-site.JPG"><img src="images/demo-site.JPG" class="left" style="width: 23%;padding-right: 10px;" /></a></p>
+
+        <p>Vestibulum pellentesque enim tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum porta leo sit amet dui sagittis, quis varius orci ultrices. Vestibulum id sagittis felis, id vulputate turpis. Donec eu nunc consequat, consectetur odio ullamcorper, suscipit nulla. Aliquam aliquam dui sit amet mauris venenatis congue. Sed in ante gravida diam malesuada iaculis. Mauris varius urna eu tellus efficitur, ac imperdiet quam sagittis. Duis in ultrices enim. Vivamus et hendrerit libero, vitae porttitor purus. Nulla euismod interdum justo, ut scelerisque enim sodales vel. Nunc ligula nisi, varius ac enim sed, rutrum fermentum urna. Quisque et rutrum odio.</p>
+
+        <p>Vivamus mollis dapibus sapien at sagittis. Mauris ac odio a purus scelerisque aliquam. Pellentesque placerat eu libero ac elementum. Sed consequat, orci et ullamcorper ullamcorper, est sem pulvinar enim, sit amet volutpat eros est non purus. Nullam iaculis venenatis sem vitae tempor. Aenean sit amet massa sit amet massa pretium dictum at nec ligula. Aenean interdum neque leo, congue fermentum metus laoreet ut. Phasellus dignissim nunc at ligula molestie mattis. Donec sit amet dolor diam. Proin ultricies metus in velit vestibulum suscipit. Aliquam id tristique nunc. Proin sed dapibus nibh, eu porttitor nisl. Nulla ac justo et lacus accumsan tempor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+
+        <div class="ribbon text-center show-for-xlarge-up">
+            <div class="ribbon-content">A Service of the Diabetes Prevention &amp; Wellness Center</div>
+        </div>
+
+    </div>
+</div>
+<div class="row">
+    <div class="columns ribbon-simple text-center show-for-large-down">A Service of the Diabetes Prevention &amp; Wellness Center</div>
+</div>
+<div class="row full connect-boxes">
+    <div class="columns medium-7 small-text-center medium-text-right connect-box">
+        <h2>Call <i class="fa fa-phone"></i></h2>
+
+        <p>Care Coordination Services: <span class="pnone-number">(901) 297-0929</span></p>
+
+        <p>Schedule a Care Coordination Appointment: <span class="pnone-number">(901) 516-9000</span></p>
+
+        <p><strong>3 Diabetes Peer Support Groups:</strong></p>
+        <ul style="list-style: none;">
+            <li>&#10102; Methodist South -
+                <span class="handwriting">Blanch&nbsp;Thomas:</span>&nbsp;<span class="pnone-number">(901) 516-3597</span>
+            </li>
+            <li>&#10103; Methodist University -
+                <span class="handwriting">Jean&nbsp;Evans:</span>&nbsp;<span class="pnone-number">(901) 516-8443</span>
+            </li>
+            <li>&#10104; Methodist North -
+                <span class="handwriting">Sheilah&nbsp;Easterling:</span>&nbsp;<span class="pnone-number">(901) 516-5617</span>
+            </li>
+        </ul>
+        <p><em>All part of the
+                <a href="http://www.methodisthealth.org/about-us/faith-and-health/community/">Congregational Health Network Navigator</a></em>
+        </p>
+    </div>
+    <div class="columns medium-5 small-only-text-center right connect-box">
+        <hr style="margin: 0 15%;" class="show-for-small" />
+
+        <h2><i class="fa fa-map-marker"></i> Visit</h2>
+
+        <p>Methodist South Medical Office Complex<br/> <a href="#" data-reveal-id="mapModal">1251 Wesley Drive, Ste. 151<br/> Memphis, TN 38118</a>
+        </p>
+
+        <div id="mapModal" class="reveal-modal" data-reveal="data-reveal">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3266.9736137919863!2d-90.020438!3d35.03238999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x7551e3d9ffcbd63a!2sMethodist+South+Hospital!5e0!3m2!1sen!2sus!4v1422892277160" width="100%" height="300" frameborder="0" style="border:0"></iframe>
+
+            <?php
+            if (strpos($_SERVER['HTTP_USER_AGENT'], 'Windows') !== false) {
+                ?>
+                <a href="bingmaps:?cp=35.03239~-90.020438" class="button show-for-large-up">Open in Desktop App</a>
+            <?php
+            }
+            if (strpos($_SERVER['HTTP_USER_AGENT'], 'Mac') !== false) {
+                ?>
+                <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button show-for-large-up">Open in App</a>
+            <?php
+            }
+            ?>
+            <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button hide-for-large-up">Open in App</a>
+
+            <a class="close-reveal-modal">&#215;</a>
+        </div>
+    </div>
+    <div class="columns medium-5 small-only-text-center right connect-box">
+        <hr style="margin: 0 15%;" class="show-for-small" />
+        <h2><i class="fa fa-clock-o"></i> Hours</h2>
+
+        <p>MON, TUE, &amp; THUR: 1pm-5pm<br/> FRI: 8am-5pm</p>
+    </div>
+</div>
+
+<!-- Footer -->
+<div class="footer contain-to-grid">
+    <footer class="row text-center">
+        <div class="large-12 columns large-centered">
+            <div class="row" data-equalizer="data-equalizer">
+
+
+                <div class="columns large-2 panel radius methodist-panel logo-panel show-for-large-up" data-equalizer-watch="data-equalizer-watch">
+                </div>
+                <div class="columns large-8 small-centered medium-uncentered large-uncentered" data-equalizer-watch="data-equalizer-watch">
+                    <div class="social-icon">
+                        <a href="http://facebook.com/#"><i class="fa fa-facebook fa-2x fa-inverse"></i></a>
+                        <a href="http://twitter.com/#"><i class="fa fa-twitter fa-2x fa-inverse"></i></a>
+                        <a href="http://instagram.com/#"><i class="fa fa-instagram fa-2x fa-inverse"></i></a>
+                        <a href="http://linkedin.com/company/#"><i
+                                class="fa fa-linkedin fa-2x fa-inverse"></i></a>&nbsp;
+                        <a href="http://pinterest.com/#/"><i class="fa fa-youtube fa-2x fa-inverse"></i></a>
+                        <a href="http://youtube.com/user/#"><i class="fa fa-pinterest fa-2x fa-inverse"></i></a>
+
+                        <p>&copy; <?php echo date("Y") ?> Diabetes Prevention &amp; Wellness Center<br/> Methodist Le Bonheur Healthcare<br/> The University of Tennessee Health Science Center
+                        </p>
+                    </div>
+                </div>
+                <div class="columns large-2 panel radius uthsc-panel logo-panel show-for-large-up" data-equalizer-watch="data-equalizer-watch">
+                </div>
+            </div>
+
+        </div>
+        <div class="columns logo-panel-group">
+            <div class="row hide-for-large-up collapse" data-equalizer="data-equalizer">
+                <div class="columns small-12 small-centered medium-uncentered medium-6 panel radius methodist-panel logo-panel" data-equalizer-watch="data-equalizer-watch">
+                </div>
+                <div class="columns small-12 small-centered medium-uncentered medium-6 panel radius uthsc-panel logo-panel" data-equalizer-watch="data-equalizer-watch">
+                </div>
+            </div>
+        </div>
+</div>
+
+</footer></div>
+
+
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/foundation/js/foundation.min.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>

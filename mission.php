a63f767257c853ef73cf8f3fe75af7a144951425
nicwinn <nic.liamg@gmail.com>
2015-03-23 09:46:24 -0500

    Created Mission, Vision, & Goals page

---
+++ mission.php
@@ -0,0 +1,46 @@
+<?php
+$pagename = 'Mission, Vision, &amp; Goals';
+include("template-top.php");
+?>
+<div class="row">
+    <div class="columns">
+        <h1><?php echo $pagename; ?></h1>
+        <div class="row panel">
+            <div class="column small-12">
+                <h2 id="mission">Our Mission</h2>
+                <p>Our mission is to provide short-term, targeted care for chronically ill patients and families in our community by working with the entire care team and empowering patients to take personal responsibility for their health.
+                </p>
+            </div>
+            <div class="columns right text-right">
+                <p>Go to Our: <a href="#vision">Vision</a> | <a href="#goals">Goals</a></p>
+            </div>
+        </div>
+        <div class="row panel">
+            <div class="column small-12">
+                <h2 id="vision">Our Vision</h2>
+                <p>Our vision is to create a culture of health and well-being in our community that will lead people to take care of their health, will help people recognize when illnesses are becoming worse, and will improve the overall health of our community.
+                </p>
+            </div>
+            <div class="columns right text-right">
+                <p style="float: right;">Go to Our: <a href="#mission">Mission</a> | <a href="#goals">Goals</a></p>
+            </div>
+        </div>
+        <div class="row panel">
+            <div class="column small-12">
+                <h2 id="goals">Our Goals</h2>
+                <ul>
+                    <li>Educate you on how to manage your chronic illness</li>
+                    <li>Help you get the care you need</li>
+                    <li>Help you manage your illness so you can stay out of the hospital and feel better</li>
+                    <li> Help you transition from the hospital to your home or other place of care</li>
+                    <li>Care for you in an environment that's patient- and family-centered</li>
+                    <li>Serves as a resource for primary care doctors in our community</li>
+                </ul>
+            </div>
+            <div class="columns right text-right">
+                <p style="float: right;">Go to Our: <a href="#mission">Mission</a> | <a href="#vision">Vision</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include("template-bottom.php"); ?>

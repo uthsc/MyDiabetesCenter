997b1d50c9224ebe4ddf3d29bb0b5e592d68109d
nicwinn <nic.liamg@gmail.com>
2015-04-16 15:44:20 -0500

    added home page content

--- index.php
+++ index.php
@@ -2,21 +2,26 @@
 $pagename = 'Home';
 include("template-top.php");
 ?>
-    <div class="row">
-        <div class="columns panel">
-            <figure class="right image-40">
-                <img src="images/methodist-building.jpg" alt="Photo of the Methodist South Hospital building"/>
-                <figcaption>Methodist South Hospital building. A 156-bed community hospital located in Whitehaven
-                    <address>1300 Wesley Dr. Memphis,TN 38116.</address>
+    <div class="row panel">
+        <div class="columns medium-8">
+
+            <p>Have you been diagnosed with diabetes? Do you need help managing your diabetes?</p>
+
+            <p>Managing diabetes is extremely important to staying healthy. But it can sometimes be challenging and hard to keep track of. My Diabetes Center is here to make sure you have the information you need to take control of diabetes.</p>
+
+            <p>We offer resources to learn about diabetes, healthy eating habits, and ways to be more active. You can also explore diabetes apps that are useful for self-management, and read about the journeys of diabetes patients just like you!</p>
+
+            <p>We hope that this will serve as a powerful resource for you to overcome diabetes….one step at a time.</p>
+
+        </div>
+        <div class="columns medium-4">
+            <h2>Explore</h2>
+            <p><strong>Stories of other people's journeys with diabetes</strong></p>
+            <figure>
+                <img src="images/douglass-hall.jpg" alt="Photo of the Douglass Hall"/>
+                <figcaption>Douglass Hall - "The doctor did initially tell me that blindness could..." <a href="stories.php#douglass-hall">read more</a>.
                 </figcaption>
             </figure>
-            <p>This homepage demo was set up using the content from the previous demo sent to me. I moved the "Contact Info" into the template footer so it could appear on all pages. Now this page is basically left with just a photo of the building. If every thing else looks good I can create the other pages based on this and look for new content for this page later.
-                <a href="images/demo-site.JPG"><img src="images/demo-site.JPG" class="left" style="width: 23%;padding-right: 10px;"/></a>
-            </p>
-
-            <p>Vestibulum pellentesque enim tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum porta leo sit amet dui sagittis, quis varius orci ultrices. Vestibulum id sagittis felis, id vulputate turpis. Donec eu nunc consequat, consectetur odio ullamcorper, suscipit nulla. Aliquam aliquam dui sit amet mauris venenatis congue. Sed in ante gravida diam malesuada iaculis. Mauris varius urna eu tellus efficitur, ac imperdiet quam sagittis. Duis in ultrices enim. Vivamus et hendrerit libero, vitae porttitor purus. Nulla euismod interdum justo, ut scelerisque enim sodales vel. Nunc ligula nisi, varius ac enim sed, rutrum fermentum urna. Quisque et rutrum odio.</p>
-
-            <p>Vivamus mollis dapibus sapien at sagittis. Mauris ac odio a purus scelerisque aliquam. Pellentesque placerat eu libero ac elementum. Sed consequat, orci et ullamcorper ullamcorper, est sem pulvinar enim, sit amet volutpat eros est non purus. Nullam iaculis venenatis sem vitae tempor. Aenean sit amet massa sit amet massa pretium dictum at nec ligula. Aenean interdum neque leo, congue fermentum metus laoreet ut. Phasellus dignissim nunc at ligula molestie mattis. Donec sit amet dolor diam. Proin ultricies metus in velit vestibulum suscipit. Aliquam id tristique nunc. Proin sed dapibus nibh, eu porttitor nisl. Nulla ac justo et lacus accumsan tempor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
         </div>
     </div>
 <?php include("template-bottom.php"); ?>

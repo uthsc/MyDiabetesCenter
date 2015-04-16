f03edf6ba5ab3744e0990f5e9f7b9a079e12846e
nicwinn <nic.liamg@gmail.com>
2015-04-16 15:44:49 -0500

    added Douglass Hall

--- stories.php
+++ stories.php
@@ -34,6 +34,7 @@ include("template-top.php");
                 </figure>
             </div>
         </div>
+
         <div id="pastor-william-burks" class="row panel">
             <h2>Pastor William Burks</h2>
             <div class="columns small-12 medium-3">
@@ -48,10 +49,35 @@ include("template-top.php");
                 <iframe width="100%" height="120" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/201114277%3Fsecret_token%3Ds-35XcB&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
                 <p>I was diagnosed with diabetes approximately 3 years ago, and to say the least it was devastating to get that information. But I decided to not let diabetes rule my life, but for me to take control of the situation.</p>
 
-                    <p>Diabetes, to me, is a journey, where in you control how far you go. You control what you do on the journey. So I have been very, very blessed in that I do check my glucose, on a daily basis, I check it periodically after I've eaten certain foods. Because I do not, I don't eat a lot of things that the average person eats. I do consume a lot of vegetables. I eat a lot of green vegetables. I eat a lot of fruit. I am specific about which fruits I eat and what time I eat them. I drink at least 96 ounces of water on a daily basis. I want you to know that this not a death sentence. Being diabetic is not a death sentence. You can do something about your condition. Make up your mind, get you a partner, someone who can work with you. Someone who will help keep you encouraged on your journey. And then what you can do is take charge of the situation. Eat right, get plenty rest, plenty of exercise, and enjoy your life—those things really make the differences in how you do as a diabetic.
+                <p>Diabetes, to me, is a journey, where in you control how far you go. You control what you do on the journey. So I have been very, very blessed in that I do check my glucose, on a daily basis, I check it periodically after I've eaten certain foods. Because I do not, I don't eat a lot of things that the average person eats. I do consume a lot of vegetables. I eat a lot of green vegetables. I eat a lot of fruit. I am specific about which fruits I eat and what time I eat them. I drink at least 96 ounces of water on a daily basis. I want you to know that this not a death sentence. Being diabetic is not a death sentence. You can do something about your condition. Make up your mind, get you a partner, someone who can work with you. Someone who will help keep you encouraged on your journey. And then what you can do is take charge of the situation. Eat right, get plenty rest, plenty of exercise, and enjoy your life—those things really make the differences in how you do as a diabetic.
                 </p>
             </div>
         </div>
+
+        <div id="douglass-hall" class="row panel">
+            <h2>Douglass Hall</h2>
+            <div class="columns small-12 medium-3 show-for-small">
+                <figure class="bio">
+                    <img src="images/douglass-hall.jpg" alt="Picture of Douglass Hall"/>
+                    <figcaption>
+                        <strong>Douglass Hall</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="column small-12 medium-9">
+                <p>The doctor did initially tell me that blindness could very well end up being one of the drawbacks if I didn’t get it under control…the biggest challenge was taking the diagnosis a little bit more seriously…when you don’t have something beating you over the head, like an open sore that might be getting worse that you can physically see, the tendency is to be a little bit dismissive…while it has been challenging, I have been able to manage this thing a whole lot better.</p>
+            </div>
+            <div class="columns small-12 medium-3 hide-for-small">
+                <figure class="bio">
+                    <img src="images/douglass-hall.jpg" alt="Picture of Douglass Hall"/>
+                    <figcaption>
+                        <strong>Douglass Hall</strong>
+                    </figcaption>
+                </figure>
+            </div>
+
+        </div>
+
     </div>
 </div>
 <?php include("template-bottom.php"); ?>

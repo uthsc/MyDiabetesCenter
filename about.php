e911163d38c8c16f1d65d44e6f86c518e0747531
nicwinn <nic.liamg@gmail.com>
2015-04-17 09:07:04 -0500

    Added Advisory Council member name list and two photos

--- about.php
+++ about.php
@@ -285,166 +285,105 @@ include("template-top.php");
         <div class="panel">
             <div class="columns medium-4 large-3">
                 <figure class="staff">
-                    <img src="images/sihoutte-man.jpg" alt="xxx"/>
+                    <img src="images/sihoutte-man.jpg" alt="picture of William Burks"/>
                     <figcaption>
-                        <strong>John Q. Public, PhD</strong><br/> Interim Dean College of University, Department Division &amp; Institution
-                        <a href="#" data-reveal-id="staff1">
-                            <address class="button tiny expand text-left">
-                                <i class="fa fa-map-marker fa-2x dropcap"></i>900 Street, #1045<br/> Memphis, TN 38104
-                            </address>
-                            <div id="staff1" class="reveal-modal" data-reveal="data-reveal">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3266.9736137919863!2d-90.020438!3d35.03238999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x7551e3d9ffcbd63a!2sMethodist+South+Hospital!5e0!3m2!1sen!2sus!4v1422892277160" width="100%" height="300" frameborder="0" style="border:0"></iframe>
-
-                                <?php
-                                if (strpos($_SERVER['HTTP_USER_AGENT'], 'Windows') !== false) {
-                                    ?>
-                                    <a href="bingmaps:?cp=35.03239~-90.020438" class="button show-for-large-up">Open in Desktop App</a>
-                                <?php
-                                }
-                                if (strpos($_SERVER['HTTP_USER_AGENT'], 'Mac') !== false) {
-                                    ?>
-                                    <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button show-for-large-up">Open in App</a>
-                                <?php
-                                }
-                                ?>
-                                <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button hide-for-large-up">Open in App</a>
-
-                                <a class="close-reveal-modal">&#215;</a>
-                            </div>
-                            <i class="fa fa-phone"></i> 901-555-6135<br/> <i class="fa fa-envelope-o"></i>
-                            <a href="mailto:email@address.edu">email@address.edu</a>
+                        <strong>William Burks</strong>
                     </figcaption>
                 </figure>
             </div>
             <div class="columns medium-4 large-3">
                 <figure class="staff">
-                    <img src="images/sihoutte-man.jpg" alt="xxx"/>
+                    <img src="images/sihoutte-man.jpg" alt="picture of Fredrick Clayton"/>
                     <figcaption>
-                        <strong>John Q. Public, PhD</strong><br/> Interim Dean College of University, Department Division &amp; Institution
-                        <a href="#" data-reveal-id="staff1">
-                            <address class="button tiny expand text-left">
-                                <i class="fa fa-map-marker fa-2x dropcap"></i>900 Street, #1045<br/> Memphis, TN 38104
-                            </address>
-                            <div id="staff1" class="reveal-modal" data-reveal="data-reveal">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3266.9736137919863!2d-90.020438!3d35.03238999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x7551e3d9ffcbd63a!2sMethodist+South+Hospital!5e0!3m2!1sen!2sus!4v1422892277160" width="100%" height="300" frameborder="0" style="border:0"></iframe>
-
-                                <?php
-                                if (strpos($_SERVER['HTTP_USER_AGENT'], 'Windows') !== false) {
-                                    ?>
-                                    <a href="bingmaps:?cp=35.03239~-90.020438" class="button show-for-large-up">Open in Desktop App</a>
-                                <?php
-                                }
-                                if (strpos($_SERVER['HTTP_USER_AGENT'], 'Mac') !== false) {
-                                    ?>
-                                    <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button show-for-large-up">Open in App</a>
-                                <?php
-                                }
-                                ?>
-                                <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button hide-for-large-up">Open in App</a>
-
-                                <a class="close-reveal-modal">&#215;</a>
-                            </div>
-                            <i class="fa fa-phone"></i> 901-555-6135<br/> <i class="fa fa-envelope-o"></i>
-                            <a href="mailto:email@address.edu">email@address.edu</a>
+                        <strong>Fredrick Clayton</strong>
                     </figcaption>
                 </figure>
             </div>
             <div class="columns medium-4 large-3">
                 <figure class="staff">
-                    <img src="images/sihoutte-man.jpg" alt="xxx"/>
+                    <img src="images/sihoutte-man.jpg" alt="picture of Mace Coday"/>
                     <figcaption>
-                        <strong>John Q. Public, PhD</strong><br/> Interim Dean College of University, Department Division &amp; Institution
-                        <a href="#" data-reveal-id="staff1">
-                            <address class="button tiny expand text-left">
-                                <i class="fa fa-map-marker fa-2x dropcap"></i>900 Street, #1045<br/> Memphis, TN 38104
-                            </address>
-                            <div id="staff1" class="reveal-modal" data-reveal="data-reveal">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3266.9736137919863!2d-90.020438!3d35.03238999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x7551e3d9ffcbd63a!2sMethodist+South+Hospital!5e0!3m2!1sen!2sus!4v1422892277160" width="100%" height="300" frameborder="0" style="border:0"></iframe>
-
-                                <?php
-                                if (strpos($_SERVER['HTTP_USER_AGENT'], 'Windows') !== false) {
-                                    ?>
-                                    <a href="bingmaps:?cp=35.03239~-90.020438" class="button show-for-large-up">Open in Desktop App</a>
-                                <?php
-                                }
-                                if (strpos($_SERVER['HTTP_USER_AGENT'], 'Mac') !== false) {
-                                    ?>
-                                    <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button show-for-large-up">Open in App</a>
-                                <?php
-                                }
-                                ?>
-                                <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button hide-for-large-up">Open in App</a>
-
-                                <a class="close-reveal-modal">&#215;</a>
-                            </div>
-                            <i class="fa fa-phone"></i> 901-555-6135<br/> <i class="fa fa-envelope-o"></i>
-                            <a href="mailto:email@address.edu">email@address.edu</a>
+                        <strong>Mace Coday</strong>
                     </figcaption>
                 </figure>
             </div>
             <div class="columns medium-4 large-3">
                 <figure class="staff">
-                    <img src="images/sihoutte-man.jpg" alt="xxx"/>
+                    <img src="images/sihoutte-man.jpg" alt="picture of Scottie Linda Fitzgerald"/>
                     <figcaption>
-                        <strong>John Q. Public, PhD</strong><br/> Interim Dean College of University, Department Division &amp; Institution
-                        <a href="#" data-reveal-id="staff1">
-                            <address class="button tiny expand text-left">
-                                <i class="fa fa-map-marker fa-2x dropcap"></i>900 Street, #1045<br/> Memphis, TN 38104
-                            </address>
-                            <div id="staff1" class="reveal-modal" data-reveal="data-reveal">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3266.9736137919863!2d-90.020438!3d35.03238999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x7551e3d9ffcbd63a!2sMethodist+South+Hospital!5e0!3m2!1sen!2sus!4v1422892277160" width="100%" height="300" frameborder="0" style="border:0"></iframe>
-
-                                <?php
-                                if (strpos($_SERVER['HTTP_USER_AGENT'], 'Windows') !== false) {
-                                    ?>
-                                    <a href="bingmaps:?cp=35.03239~-90.020438" class="button show-for-large-up">Open in Desktop App</a>
-                                <?php
-                                }
-                                if (strpos($_SERVER['HTTP_USER_AGENT'], 'Mac') !== false) {
-                                    ?>
-                                    <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button show-for-large-up">Open in App</a>
-                                <?php
-                                }
-                                ?>
-                                <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button hide-for-large-up">Open in App</a>
-
-                                <a class="close-reveal-modal">&#215;</a>
-                            </div>
-                            <i class="fa fa-phone"></i> 901-555-6135<br/> <i class="fa fa-envelope-o"></i>
-                            <a href="mailto:email@address.edu">email@address.edu</a>
+                        <strong>Scottie Linda Fitzgerald</strong>
                     </figcaption>
                 </figure>
             </div>
             <div class="columns medium-4 large-3">
                 <figure class="staff">
-                    <img src="images/sihoutte-man.jpg" alt="xxx"/>
+                    <img src="images/sihoutte-man.jpg" alt="picture of Mary Lou Gutierrez"/>
                     <figcaption>
-                        <strong>John Q. Public, PhD</strong><br/> Interim Dean College of University, Department Division &amp; Institution
-                        <a href="#" data-reveal-id="staff1">
-                            <address class="button tiny expand text-left">
-                                <i class="fa fa-map-marker fa-2x dropcap"></i>900 Street, #1045<br/> Memphis, TN 38104
-                            </address>
-                            <div id="staff1" class="reveal-modal" data-reveal="data-reveal">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3266.9736137919863!2d-90.020438!3d35.03238999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x7551e3d9ffcbd63a!2sMethodist+South+Hospital!5e0!3m2!1sen!2sus!4v1422892277160" width="100%" height="300" frameborder="0" style="border:0"></iframe>
-
-                                <?php
-                                if (strpos($_SERVER['HTTP_USER_AGENT'], 'Windows') !== false) {
-                                    ?>
-                                    <a href="bingmaps:?cp=35.03239~-90.020438" class="button show-for-large-up">Open in Desktop App</a>
-                                <?php
-                                }
-                                if (strpos($_SERVER['HTTP_USER_AGENT'], 'Mac') !== false) {
-                                    ?>
-                                    <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button show-for-large-up">Open in App</a>
-                                <?php
-                                }
-                                ?>
-                                <a href="http://maps.apple.com/?q=35.03239,-90.020438" class="button hide-for-large-up">Open in App</a>
-
-                                <a class="close-reveal-modal">&#215;</a>
-                            </div>
-                            <i class="fa fa-phone"></i> 901-555-6135<br/> <i class="fa fa-envelope-o"></i>
-                            <a href="mailto:email@address.edu">email@address.edu</a>
+                        <strong>Mary Lou Gutierrez</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="columns medium-4 large-3">
+                <figure class="staff">
+                    <img src="images/douglass-hall.jpg" alt="picture of Douglass Hall"/>
+                    <figcaption>
+                        <strong>Douglass Hall</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="columns medium-4 large-3">
+                <figure class="staff">
+                    <img src="images/sihoutte-man.jpg" alt="picture of Leona Marshall"/>
+                    <figcaption>
+                        <strong>Leona Marshall</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="columns medium-4 large-3">
+                <figure class="staff">
+                    <img src="images/sihoutte-man.jpg" alt="picture of Leona Marshall"/>
+                    <figcaption>
+                        <strong>Leona Marshall</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="columns medium-4 large-3">
+                <figure class="staff">
+                    <img src="images/sihoutte-man.jpg" alt="picture of Arsenio Murrell"/>
+                    <figcaption>
+                        <strong>Arsenio Murrell</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="columns medium-4 large-3">
+                <figure class="staff">
+                    <img src="images/ruthie-tate.jpg" alt="picture of Ruthie Tate"/>
+                    <figcaption>
+                        <strong>Ruthie Tate</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="columns medium-4 large-3">
+                <figure class="staff">
+                    <img src="images/sihoutte-man.jpg" alt="picture of Blanch Thomas"/>
+                    <figcaption>
+                        <strong>Blanch Thomas</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="columns medium-4 large-3">
+                <figure class="staff">
+                    <img src="images/sihoutte-man.jpg" alt="picture of Margaret Toles"/>
+                    <figcaption>
+                        <strong>Margaret Toles</strong>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="columns medium-4 large-3">
+                <figure class="staff">
+                    <img src="images/sihoutte-man.jpg" alt="picture of Sandra Williams"/>
+                    <figcaption>
+                        <strong>Sandra Williams</strong>
                     </figcaption>
                 </figure>
             </div>

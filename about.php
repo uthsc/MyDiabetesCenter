6d5e7aa8ffd95454c7ea8fa23353c97e71d922a0
Nic Winn <nic.liamg@gmail.com>
2015-06-30 10:14:41 -0500

    removed proceeding character from paragraph

--- about.php
+++ about.php
@@ -5,7 +5,7 @@ include("template-top.php");
 <div id="content" class="row">
     <div class="columns">
         <h1><?php echo $pagename; ?></h1>
-        <p>IThe Diabetes Wellness and Prevention Coalition is a patient, provider, and research partnership started in 2009 by Dr. Michael Ugwueke, then CEO of Methodist South Hospital, to improve diabetes care by emphasizing "prevention through proactive disease management…shift[ing] the treatment paradigm to the outpatient arena…thus…reducing the incidence and severity of the complications experienced by the patient, leading to an increase in quality of life." The organization and key partners in the Coalition are shown below:</p>
+        <p>The Diabetes Wellness and Prevention Coalition is a patient, provider, and research partnership started in 2009 by Dr. Michael Ugwueke, then CEO of Methodist South Hospital, to improve diabetes care by emphasizing "prevention through proactive disease management…shift[ing] the treatment paradigm to the outpatient arena…thus…reducing the incidence and severity of the complications experienced by the patient, leading to an increase in quality of life." The organization and key partners in the Coalition are shown below:</p>
         <figure>
             <img src="images/dwpc-coalition-key-partners.jpg" width="100%" alt="image of the dwpc coalition key partners" />
         </figure>

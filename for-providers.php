63bdcb0a7aeee24602c4531dad45e19e25fd8f10
Nic Winn <nic.liamg@gmail.com>
2017-02-17 13:37:28 -0600

    new page

---
+++ for-providers.php
@@ -0,0 +1,67 @@
+<?php
+$pagename = 'For Providers';
+include("template-top.php");
+?>
+<div id="content" class="row">
+    <div class="columns">
+        <h1><?php echo $pagename; ?></h1>
+        <h2>Join us in the effort to improve care in the Mid-South!</h2>
+        <p>The Diabetes Wellness and Prevention Coalition (DWPC) in collaboration with the University of Tennessee Health Science Center operates the <i>DWPC Registry</i> to track processes and outcomes of care and improve care for people living with diabetes or at risk for diabetes in the Mid-South. The <i>DWPC Registry</i> fully qualifies as a Specialized Registry for the purposes of achieving meaningful use of electronic health records. We have begun accepting data as a specialized registry and will be using the data to improve population health outcomes.  The <i>DWPC Registry</i> also undergirds our practice improvement work through the Patient-Centered Outcomes Research Institute (PCORI)-funded <i>Management Of Diabetes in Everyday Life (MODEL) Program</i>. We also provide practice feedback of quarterly practice improvement reports to participating providers.</p>
+        <figure class="bio">
+            <img src="images/diabetes-wellness-and-prevention-coalition-registry-schematic.jpg" alt="Picture of Diabetes Wellness and Prevention Coalition Registry Schematic. Three silos of text flowing into one combined silo
+First silo heading - Outpatient Electronic Medical Records
+First silo content -
+Cerner Ambulatory
+EClinicals
+Billing Data
+Prescription Data
+Laboratory Data
+
+Second silo heading - Hospital Electronic Medical Records
+Second silo content -
+Cerner
+Inpatient
+Billing Data
+Prescription Data
+Laboratory Data
+
+Third silo heading - Health Insurance & Pharmacy Claims
+Third silo content -
+BlueCross BlueShield of Tennessee
+United HealthCare
+Tennessee Medicaid (TennCare)
+Medicare
+
+Combined silo information heading - Motivational Mobile Messages Program–Data Management System
+Combined silo information Content -
+Patient-reported:
+Demographics
+Cell phone type
+Internet-connected computer access
+Characteristics
+Treatment self-regulation
+Perceived competency
+Barriers to self-care
+Health literacy
+Patient preferences
+Self-care focus or foci
+Message & visit frequency
+Support group participation
+Outcomes
+Diabetes self-care
+Quality of life
+Primary care engagement"/>
+            <figcaption>
+                Diabetes Wellness and Prevention Coalition Registry Schematic
+            </figcaption>
+        </figure>
+        <p>We are able to receive electronic data generated from certified electronic health record technology (CEHRT) through appropriate secure mechanisms. The <i>DWPC Registry</i> has a registration of intent process that takes clinics and other providers through test and validation and a process to move into production. We provide participating providers with appropriate documentation in order to begin this process and we notify them by email on a regular basis of the status of their feeds to the registry and whether they are currently meeting our criteria for "Active Engagement".</p>
+        <div class="panel">
+        <p>To participate in the <i>DWPC Registry</i>, or to request more information, contact:<br />
+            <strong>Bonnie Binkley, MA</strong><br />
+            <a href="mailto:info@mydiabetescenter.org">info@mydiabetescenter.org</a><br />
+            (901) 448-4168</p>
+        </div>
+    </div>
+</div>
+<?php include("template-bottom.php"); ?>

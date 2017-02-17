cfb45d18a57552a99bc49aec76991c450b21a18d
Nic Winn <nic.liamg@gmail.com>
2017-02-17 13:38:59 -0600

    added link in nav to new page

--- template-top.php
+++ template-top.php
@@ -84,6 +84,7 @@
                 <li><a href="services.php">Services</a></li>
                 <li><a href="start.php">Get Started</a></li>
                 <li><a href="stories.php">Stories</a></li>
+                <li><a href="for-providers.php">For Providers</a></li>
             </ul>
         </section>
     </nav>

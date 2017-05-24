77e49954b7ec4740a13f8706f2e9cba8506fee0c
Nic Winn <nic.liamg@gmail.com>
2017-05-24 14:35:15 -0500

    new page

--- the-model-study.php
+++ the-model-study.php
@@ -8,27 +8,75 @@ include("template-top.php");
         <h1><?php echo $pagename; ?></h1>
 
         <div class="row panel">
-            <div class="column small-12">
-                <p>Our highly skilled care team and provider partners can help you understand and manage your diabetes. We provide:
-                </p>
+            <div class="columns small-12">
+                <h3 class="text-center">Take part in an important research study aimed at<br />helping people with diabetes take better care of
+                    their health</h3>
+                <img src="images/model-logo.png" style="padding: 2vw 5vw 5vw 5vw;" />
+                <br class="hide-for-small" style="clear: both;" />
+            </div>
+
+            <div class="columns medium-1">
+                <br class="hide-for-small" style="clear: both;" />
+
+            </div>
+            <div class="columns medium-5">
+                <br class="hide-for-small" style="clear: both;" />
+                <h4 class="text-center">We are looking for African-American men and women over the age of 18 who:</h4>
                 <ul>
-                    <li>Regular Comprehensive Exams</li>
-                    <li>Education on how to manage your condition</li>
-                    <li>Help connecting to resources</li>
-                    <li>Tools to monitor and manage your condition</li>
-                    <li>Help communicating with your doctor</li>
-                    <li>Help understanding and using benefits provided by health plan and community agencies</li>
-                    <li>Personal check-up calls between visits</li>
-                </ul>
-                <h3>Contact our provider partners listed below for an appointment today:</h3>
-                <ul class="small-block-grid-2">
-                    <li><strong>UT Methodist Physicians</strong><br />1251 Wesley Dr. #151, Memphis, TN 38118<br /><i class="fa fa-phone"></i> 901-758-7888</li>
-                    <li><strong>Motley Internal Medicine Group</strong><br />1264 Wesley Dr #606, Memphis, TN 38118<br /><i class="fa fa-phone"></i> 901-332-2277</li>
-                    <li><strong>TriState Medical Group, PLLC</strong><br />1264 Wesley Dr #405, Memphis, TN 38118<br /><i class="fa fa-phone"></i> 901-332-9632</li>
-                    <li><strong>Christ Community Health Services</strong><br />Third Street Clinic, 3362 S. Third St., Memphis, TN 38118<br /><i class="fa fa-phone"></i> 901-701-2510</li>
+                    <li>have diabetes with high blood sugars</li>
+                    <li>have a cell phone with text message capability</li>
                 </ul>
             </div>
+            <div class="columns medium-1">
+                <br class="hide-for-small" style="clear: both;" />
+
+            </div>
+
+            <div class="columns medium-5">
+                <ul class="pricing-table">
+                    <li class="title">Join</li>
+                    <li class="price">$150</li>
+                    <li class="description">The study lasts one year, and you will receive up to <strong>$150</strong>
+                        for follow-up visits.
+                    </li>
+                    <li class="bullet-item"><span class="fa fa-phone"></span> (901) 448-1381</li>
+                    <li class="cta-button"><a class="button" href="mailto:info@mydiabetescenter.org"><span class="fa fa-envelope"></span> Email Now</a></li>
+                </ul>
+            </div>
+            <br  style="clear: both;" />
+            <div class="row panel callout radius text-center collapse">
+                <h4><span class="fa fa-stethoscope"></span> Participating Clinics</h4>
+                <hr/>
+                <div class="columns medium-4">
+                    <p>
+                        Christ Community - Raleigh<br/>
+                        Christ Community - Third Street<br/>
+                        Tristate Medical Group<br/>
+                        UT Methodist Physicians - South
+                    </p>
+                </div>
+                <div class="columns medium-4">
+                    <p>
+                        Hawkins Family Medicine<br/>
+                        UT Family Practice - Jackson<br/>
+                        UT Family Practice - Tipton
+                    </p>
+                </div>
+                <div class="columns medium-4">
+                    <p>
+                        Methodist Medical Group - Primary&nbsp;Care<br/>
+                        1325 Eastmoreland, Suite 245<br/>
+                        1264 Wesley Drive, Suite 606
+                    </p>
+                </div>
+            </div>
         </div>
+    </div>
+    <div class="columns medium-text-center">
+    <ul class="vcard" style="border: none;">
+
+        <li><span class="fn"><span class="subheader">Investigator:</span> Dr.&nbsp;Jim&nbsp;Bailey</span> <span class="street-address>">956&nbsp;Court&nbsp;Avenue,&nbsp;D222</span> <span class="locality">Memphis</span>,&nbsp;<span class="state">TN</span>&nbsp;<span class="zip">38163</span></li>
+    </ul>
     </div>
 </div>
 <?php include("template-bottom.php"); ?>

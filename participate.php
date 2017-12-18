35eabadcd77b4197b84803894765c867cb735fa8
Nic Winn <nic.liamg@gmail.com>
2017-12-18 13:01:43 -0600

    updated contacts

--- participate.php
+++ participate.php
@@ -15,7 +15,7 @@ include("template-top.php");
                             <figure class="bio">
                                 <img src="images/ruthie-tate.jpg" alt="Picture of Ruthie Tate"/>
                                 <figcaption>
-                                    <strong>Ruthie Tate, Co-Chair</strong><br/>901-849-4561<br /><a href="mailto:ruthieltate@yahoo.com">ruthieltate@yahoo.com</a>
+                                    <strong>Ruthie Tate, Co&#x2011;Chair</strong><br/>(901)&nbsp;849&#x2011;4561<br /><a href="mailto:ruthieltate@yahoo.com">ruthieltate@yahoo.com</a>
                                 </figcaption>
                         </div>
                         <div class="columns medium-6">
@@ -23,7 +23,7 @@ include("template-top.php");
                             <figure class="bio">
                                 <img src="images/douglass-hall.jpg" alt="Picture of Douglass Hall"/>
                                 <figcaption>
-                                    <strong>Douglass Hall, Co-Chair</strong><br/>901-644-9698<br /><a href="mailto:doughallc@gmail.com">doughallc@gmail.com</a>
+                                    <strong>Douglass Hall, Co&#x2011;Chair</strong><br/>(901)&nbsp;644&#x2011;9698<br /><a href="mailto:doughallc@gmail.com">doughallc@gmail.com</a>
                                 </figcaption>
                             </figure>
                         </div>
@@ -38,8 +38,8 @@ include("template-top.php");
                 <h2 id="cac">Community Advisory Council (CAC)</h2>
                 <p>The CAC meets quarterly to make sure Diabetes Wellness & Prevention Center programs help address true community needs and take full advantage of existing community resources. The CAC also works to spread and share successful Coalition programs with other people and communities working to overcome diabetes one step at a time. To participate, or for more information, please contact:</p>
                 <ul>
-                    <li>Delois Broady, Co-Chair - <a href="mailto:delois.broady@mlh.org">delois.broady@mlh.org</a></li>
-                    <li>Phebe Mclin, Co-Chair - <a href="mailto:phebe.mclin@gmail.com">phebe.mclin@gmail.com</a> </li>
+                    <li>Delois Broady, Co&#x2011;Chair&ensp;&ndash;&ensp;<a href="mailto:delois.broady@mlh.org">delois.broady@mlh.org</a></li>
+                    <li>Phebe Mclin, Co&#x2011;Chair&ensp;&ndash;&ensp;<a href="mailto:phebe.mclin@gmail.com">phebe.mclin@gmail.com</a> </li>
                 </ul>
             </div>
             <div class="columns right text-right">
@@ -51,8 +51,8 @@ include("template-top.php");
                 <h2 id="plc">Provider Learning Collaborative (PLC)</h2>
                 <p>The PLC meets bi-monthly to work together through the Whitehaven DWPC to improve diabetes care and promote wellness for people with diabetes or at risk for diabetes and its many complications in the Whitehaven area.  Through the PLC, providers learn and share: 1) best practices in diabetes care; 2) practice data on diabetes care performance; 3) diabetes practice improvement methods; and 4) ways to identify and overcome barriers to best diabetes care. To participate, or for more information, please contact:</p>
                 <ul>
-                    <li>Dr. Helmut Steinberg, Co-Chair - <a href="mailto:hsteinb1@uthsc.edu">hsteinb1@uthsc.edu</a>, 901-448-5240</li>
-                    <li>Dr. Todd Motley, Co-Chair - <a href="mailto:motleyintermed@bellsouth.net">motleyintermed@bellsouth.net</a> (or contact Vicky Nelson - <a href="mailto:Vicky.Bonner@mlh.org">Vicky.Bonner@mlh.org</a>, 901-332-8547)</li>
+                    <li>Dr. Stanley Dowell, Co&#x2011;Chair&ensp;&ndash;&ensp;<a href="mailto:stanley.dowell@mlh.org">stanley.dowell@mlh.org</a><br />&emsp;or:&ensp;Eileen Thomas&ensp;&ndash;&ensp;<a href="mailto:eileen.thomas@mlh.org">eileen.thomas@mlh.org</a></li>
+                    <li>Dr. Helmut Steinberg, Co&#x2011;Chair&ensp;&ndash;&ensp;<a href="mailto:hsteinb1@uthsc.edu">hsteinb1@uthsc.edu</a>, (901)&nbsp;448&#x2011;5240</li>
                 </ul>
             </div>
             <div class="columns right text-right">
@@ -64,8 +64,8 @@ include("template-top.php");
                 <h2 id="steering">Steering Committee</h2>
                 <p>The Steering Committee meets quarterly to oversee, direct, and help implement the programs and activities of the Diabetes Wellness and Prevention Center. To participate, or for more information, please contact:</p>
                 <ul>
-                    <li>Dr. James Robinson, Co-Chair - <a href="mailto:James.Robinson@mlh.org">James.Robinson@mlh.org</a> (or contact Sibyl Meyers-Williams - <a href="mailto:Sibyl.Meyers-Williams@mlh.org">Sibyl.Meyers-Williams@mlh.org</a> , 901-516-3081)</li>
-                    <li>Dr. Jim Bailey, Co-Chair – <a href="mailto:jeb@uthsc.edu">jeb@uthsc.edu</a>, 901-448-2475</li>
+                    <li>Dr. James Robinson, Co&#x2011;Chair&ensp;&ndash;&ensp;<a href="mailto:James.Robinson@mlh.org">James.Robinson@mlh.org</a><br />&emsp;or:&ensp;Sibyl Meyers-Williams&ensp;&ndash;&ensp;<a href="mailto:Sibyl.Meyers-Williams@mlh.org">Sibyl.Meyers&#x2011;Williams@mlh.org</a> , (901)&nbsp;516&#x2011;3081</li>
+                    <li>Dr. Jim Bailey, Co&#x2011;Chair&ensp;&ndash;&ensp;<a href="mailto:jeb@uthsc.edu">jeb@uthsc.edu</a>, (901)&nbsp;448&#x2011;2475</li>
                 </ul>
             </div>
             <div class="columns right text-right">
